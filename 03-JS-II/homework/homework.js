4af1a0ab146de7c83125a56c76c8508e196ece55
Patriciopg02 <patriciopg02_@hotmail.com>
2022-05-17 21:45:34 -0300

    homework_JS2v2

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -148,9 +148,9 @@ function fizzBuzz(numero) {
     else return 'fizz';
   }
   else if ((numero % 5) == 0) {
-    return 'buzz'
+    return 'buzz';
   }
-  else return numero
+  else return numero;
 }
 
 function operadoresLogicos(num1, num2, num3) {
@@ -161,21 +161,21 @@ function operadoresLogicos(num1, num2, num3) {
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
   if (num1 == 0 || num2 == 0 || num3 == 0) {
-    return 'Error'
+    return 'Error';
   }
   else if (num1 < 0 || num2 < 0 || num3 < 0) {
-    return 'Hay negativos'
+    return 'Hay negativos';
   }
   else {
     if (num1 > num2 && num1 > num3 && num1 > 0) {
-      return 'Numero 1 es mayor y positivo'
+      return 'Numero 1 es mayor y positivo';
     }
     else if (num3 > num1 && num3 > num2) {
-      num3 = num3 + 1
-      return num3
+      num3 = num3 + 1;
+      return num3;
     }
     else {
-      return false
+      return false;
     }
   }
 }
